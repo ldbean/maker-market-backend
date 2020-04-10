d3eb4fa97bd4a1d8bd800c1f408b4d34d46ae8dc
leighannbaxa <leighannbaxa@gmail.com>
2020-04-10 15:34:20 -0500

    includes verifyToken middleware for post routes

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,10 @@ const verifyToken = require('../middleware/verification');
 
 
 // posts
-router.get('/posts', verifyToken, ctrl.posts.index);
-router.get('/posts/:id', ctrl.posts.show);
-router.post('/posts', ctrl.posts.create);
-router.put('/posts/:id', ctrl.posts.update);
-router.delete('/posts/:id', ctrl.posts.destroy);
+router.get('/posts', ctrl.posts.index);
+router.get('/posts/:id', verifyToken, ctrl.posts.show);
+router.post('/posts', verifyToken, ctrl.posts.create);
+router.put('/posts/:id', verifyToken, ctrl.posts.update);
+router.delete('/posts/:id', verifyToken, ctrl.posts.destroy);
 
 module.exports = router;
